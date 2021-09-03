ba1defcfba9346d05490173faf49dd9b1f40bcf7
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-03 15:27:27 +0200

    check numeri doppi + stampa risultato

--- main.js
+++ main.js
@@ -4,23 +4,31 @@ function getRandom(min, max) {
 
 
 let lista = []
+let listaSi = []
+let listaNo = []
 
 
 for(let i=0;i<5;i++){
-    lista.push(" " + getRandom(1,300))
+    lista.push(getRandom(1,300))
 }
 
 alert(lista)
-console.log(lista);
+
 
 for(let b=0;b<lista.length;b++){
     setTimeout(function() {
-        let num = prompt("inserisci i numeri");
-
-        if(lista[b].includes(num) == true){
-            console.log("ok");
-        }else{
-            console.log("no");
+        let num = parseInt(prompt("inserisci i numeri"));
+        while(listaSi.includes(num) || listaNo.includes(num)){
+            alert("no")
+            num = parseInt(prompt("inserisci i numeri"));
         }
-    }, 500);
+            if(lista.includes(num)){
+                listaSi.push(num)
+            }else{
+                listaNo.push(num)
+            }
+            document.getElementById("corretto").innerHTML = listaSi.length
+            document.getElementById("errore").innerHTML = listaNo.length
+        }, 1000);
 }
+
